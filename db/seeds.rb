488f8b235588fbe60aeef42b84bb10b8dee4814e
Kazuki-Ninamigawa <minamigawa.kazuki.infratop@gmail.com>
2020-05-31 14:24:13 +0900

    実装課題5

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create!(
   email: "admin@example.com",
-  encrypted_password: "wwwwww"
+  password: "wwwwww"
 )
 Genre.create!(
   name: "ジャンル名",
@@ -17,24 +17,4 @@ Item.create!(
   name: "商品名",
   introduction: "商品説明文",
   no_tax_price: 500,
-)
-Order.create!(
-  end_user_id: 1,
-  address: "東京都",
-  zip_code: "0000000",
-  name: "南川和紀",
-  payment_method: 0,
-)
-Address.create!(
-  end_user_id: 1,
-  address: "大阪府",
-  zip_code: "1111111",
-  name: "大阪太郎"
-)
-OrderDetail.create!(
-  order_id: 1,
-  item_id: 1,
-  item_amount: 1,
-  tax_price: 500,
-  made_process: 0
 )
